fe966dcd8458943e1c05fe67e10bb6ae18987400
Ajaykumaryadav-Aj <ajaykumarsiwan182@gmail.com>
2024-04-16 01:48:20 +0530

    home screen

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,83 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        systemOverlayStyle:
+            const SystemUiOverlayStyle(statusBarBrightness: Brightness.dark),
+      ),
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        child: Stack(
+          children: [
+            Align(
+              alignment: const AlignmentDirectional(3, -0.3),
+              child: Container(
+                height: 300,
+                width: 300,
+                decoration: const BoxDecoration(
+                    shape: BoxShape.circle, color: Colors.deepPurple),
+              ),
+            ),
+            Align(
+              alignment: const AlignmentDirectional(-3, -0.3),
+              child: Container(
+                height: 300,
+                width: 300,
+                decoration: const BoxDecoration(
+                    shape: BoxShape.circle, color: Color(0xFF673AB7)),
+              ),
+            ),
+            Align(
+              alignment: const AlignmentDirectional(0, -1.2),
+              child: Container(
+                height: 300,
+                width: 600,
+                decoration: const BoxDecoration(color: Color(0xFFFFAB40)),
+              ),
+            ),
+            BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 100, sigmaY: 100),
+              child: Container(
+                decoration: const BoxDecoration(color: Colors.transparent),
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              child: const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Aix-en-Provence',
+                    style: TextStyle(
+                        fontWeight: FontWeight.w300, color: Colors.white),
+                  ),
+                  SizedBox(
+                    height: 7,
+                  ),
+                  Text(
+                    'Good Morning',
+                    style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 25,
+                        color: Colors.white),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
